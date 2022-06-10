8634ed888290ed160feb8834e2631af764c7c385
pavan <pavandotnet007@outlook.com>
2022-06-10 21:28:53 +0530

    converted to nextjs

--- src/components/collectionsItems/Item.tsx
+++ src/components/collectionsItems/Item.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { getFormattedCurrency } from '../../utils/getFormattedCurrency';
 import { Product } from './CollectionsItems';
-import { useState } from 'react';
 
 type Props = {
 	products: Product[];
@@ -9,8 +9,6 @@ type Props = {
 
 const Item = (props: Props) => {
 	const { products } = props;
-	const [isVisible, setIsVisible] = useState(true);
-
 	return (
 		<>
 			{products &&
@@ -18,14 +16,16 @@ const Item = (props: Props) => {
 					const { _id, imageURL, brand, category, name, amount } = product;
 					const formattedAmount = getFormattedCurrency(amount);
 					return (
-						<div key={_id} className=' border-[1px] rounded text-sm w-full '>
-							<Image src={imageURL} alt={name} width={220} height={275} layout='responsive' priority={index < 8} />
-							<div className=' p-2 space-y-1 flex-col flex justify-between bg-gray-200   '>
-								<p>{brand}</p>
-								<p className=' text-gray-400'>{name}</p>
-								<p>Rs.{formattedAmount}</p>
-							</div>
-						</div>
+						<Link href='/' key={_id}>
+							<a className=' border-[1px] shadow overflow-hidden rounded text-sm w-full '>
+								<Image src={imageURL} alt={name} width={220} height={275} layout='responsive' priority={index < 8} />
+								<div className=' p-2 space-y-1 flex-col flex justify-between    '>
+									<p>{brand}</p>
+									<p className=' text-gray-400'>{name}</p>
+									<p>Rs.{formattedAmount}</p>
+								</div>
+							</a>
+						</Link>
 					);
 				})}
 		</>
