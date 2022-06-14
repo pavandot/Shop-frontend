d89333cc3b3db119b9dc519bf2887669193ad02b
pavan <pavandotnet007@outlook.com>
2022-06-14 16:19:54 +0530

    added product page

--- src/components/collectionsItems/Item.tsx
+++ src/components/collectionsItems/Item.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 import { getFormattedCurrency } from '../../utils/getFormattedCurrency';
 import { Product } from './CollectionsItems';
 
-interface Props  {
+interface Props {
 	products: Product[];
-};
+}
 
 const Item = (props: Props) => {
 	const { products } = props;
@@ -16,7 +16,7 @@ const Item = (props: Props) => {
 					const { _id, imageURL, brand, category, name, amount } = product;
 					const formattedAmount = getFormattedCurrency(amount);
 					return (
-						<Link href='/' key={_id}>
+						<Link href={`/collections/${_id}`} key={_id}>
 							<a className=' border-[1px] shadow overflow-hidden rounded text-sm w-full '>
 								<Image src={imageURL} alt={name} width={220} height={275} layout='responsive' priority={index < 8} />
 								<div className=' p-2 space-y-1 flex-col flex justify-between    '>
