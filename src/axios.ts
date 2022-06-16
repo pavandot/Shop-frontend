ed1fb674b43c62495ceffb37239debfbf586a886
pavan <pavandotnet007@outlook.com>
2022-06-16 13:53:17 +0530

    production url updated

--- src/axios.ts
+++ src/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const PRODUCTION_URL = 'https://pavan-shop.herokuapp.com';
 const DEVELOPMENT_URL = 'http://localhost:5000';
 const instance = axios.create({
-	baseURL: DEVELOPMENT_URL,
+	baseURL: PRODUCTION_URL,
 });
 
 export default instance;
