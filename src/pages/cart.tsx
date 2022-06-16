ac122d0879aeb1980b15eb4605d24d8014ec7fbb
pavan <pavandotnet007@outlook.com>
2022-06-16 13:30:07 +0530

    add to cart feature added

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,10 +1,32 @@
+import { useQueryClient } from 'react-query';
+import { Product } from '../components/collectionsItems/CollectionsItems';
+import ErrorModal from '../components/ErrorModal';
+import Loading from '../components/Loading';
 import useGetCartItems from '../hooks/useGetCartItems';
-
+export interface CartItem {
+	_id: string;
+	product: Product;
+	size: string;
+	quantity: number;
+}
 const Cart = () => {
-	const { data, isSuccess } = useGetCartItems();
-	if (isSuccess) console.log(data);
-
-	return <div>cart</div>;
+	const queryClint = useQueryClient();
+	const { data, isSuccess, isLoading, isError, error } = useGetCartItems();
+	const cartQuantity = queryClint.getQueryData('cartQuantity');
+	if (isLoading) return <Loading />;
+	if (isError) return <ErrorModal message='Something went wrong' open={true} />;
+	return (
+		<>
+			{isSuccess && data && (
+				// <section className='max-w-3xl mt-5  px-2 sm:px-5 2xl:px-0 mx-auto'>
+				// 	<p className=' text-lg font-medium'>
+				// 		Cart <span className=' text-sm md:text-base text-gray-500'>{`(${cartQuantity} items)`}</span>
+				// 	</p>
+				// </section>
+				<p>lol</p>
+			)}
+		</>
+	);
 };
 
 export default Cart;
